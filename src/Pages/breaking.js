a7aff7610f8bd27ce75e919b690bafe98b6f4951
Samantha Buonato <sb2024@icloud.com>
2023-12-10 21:34:40 -0500

    got photos to appear

--- src/Pages/breaking.js
+++ src/Pages/breaking.js
@@ -3,8 +3,11 @@ import axios from 'axios';
 import { queries } from '@testing-library/react';
 
 const Breaking = () => {
+    
+    const [image, setImage]=useState('')
     const [quote, setQuote]=useState('')
     const [author, setAuthor]=useState('')
+
     useEffect(()=>{
         axios.get('https://api.breakingbadquotes.xyz/v1/quotes')
             .then(res=>{
@@ -16,22 +19,33 @@ const Breaking = () => {
                 console.log(err);
             })
     },[])
-    const options = {
-        method: 'GET',
-        url: 'https://wikimedia-image-search.p.rapidapi.com/wiki/',
-        params: {
-          query: 'Jesse Pinkman'
-        }
-    }
-        try {
-            res = axios.request(options);
-            consolelog(res.data)
-        } 
-        catch (error) {
-            console.log(err);
-        }
 
+    const apiKey = 'AIzaSyDhFQi2bNa9sDcED_7V6Z_R6kUCuzHExUI';
+    const cx = 'e483e623c5c7e4d9f';
+    const query = '${author} actorfrom breaking bad';
+    const apiUrl = `https://www.googleapis.com/customsearch/v1?q=${query}&cx=${cx}&key=${apiKey}&searchType=image`;
+    
+    useEffect(()=>{
+    axios.get(apiUrl)
+        .then(res => {
+            console.log(res);
+            setImage(res.data.items[0].link);
+        })
+        .catch(err => {
+            console.log(err);
+        });
+        }, [apiUrl]);
+    
+    return (
+        <div>
+            <h1>Breaking Bad Quote:</h1>
+            <p>{quote}</p>
+            <p>{author}</p>
+            {image && <img src={image} alt={'author image'} style={{ maxWidth: '200px', borderRadius: '0%' }} />}
+        </div>
+    )
     
 }
 
 export default Breaking;
+
